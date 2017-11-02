1807da8cd0098ea5150a7b14985292c38c718dd3
Bryan Cairns <bcairns@voidrealms.com>
2017-11-02 15:40:00 -0400

    First commit

---
+++ VB 15/ConsApp hashtable 15/Module1.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+
+    Sub Main()
+
+        'hash Table
+
+        Dim employees As Hashtable = New Hashtable
+
+        employees.Add(102, "bob smith")
+        employees.Add(105, "bob hope")
+
+        If employees.ContainsKey(105) Then
+            Console.WriteLine(employees.Item(105) & " exists")
+
+        End If
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
---
+++ readme.txt
@@ -0,0 +1,10 @@
+Code for video tutorial
+
+Title: Visual basic 15 - Hash Table
+Published: 7/11/2010 12:05:57 PM
+Link: https://www.youtube.com/watch?v=C1iHsewww_8
+
+Description: Learn about the Hash Table
+
+If you found this helpful, please consider donating: http://www.voidrealms.com
+
